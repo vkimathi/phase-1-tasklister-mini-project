8c0a80938c84de20dab340de3b4b0aeb2d134ebb
Victor Kimathi <vkimathi97@gmail.com>
2025-04-10 23:04:50 +0300

    tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    handleToDo(event.target['new-task-description'].value);
+    form.reset()
+})
 });
+
+const handleToDo = (todo) => {
+  let li = document.createElement('li')
+  let btn = document.createElement('Button')
+  btn.addEventListener('click', deleteToDo)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  // console.log(li);
+  document.getElementById('tasks').appendChild(li)
+}
+
+const deleteToDo = (event) => {
+  event.target.parentNode.remove()
+}
